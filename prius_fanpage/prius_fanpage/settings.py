567895154fe12f29c706990a386fc40156a5824f
pcibiras <122815299+pcibiras@users.noreply.github.com>
2023-03-19 19:31:58 +0200

    Update settings.py

--- prius_fanpage/prius_fanpage/settings.py
+++ prius_fanpage/prius_fanpage/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$7#=2r4c$7qw2g_0hfmchpmfnqj8!bhm%mn(lm3yu_8hn96+1e'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
